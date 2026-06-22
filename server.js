f1f14f2610b4cea16bea492ca928ad4ad52c9ca2
Идиакант <kushner.maksim.v@gmail.com>
2026-06-22 17:18:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors: { origin: "*" } // Разрешаем подключения из любой точки мира
+    cors: { origin: "*" }
+});
+
+// ЧИТАЙ ТУТ: Поместить файл index.html в ту же папку, что и server.js на Render
+app.use(express.static(__dirname));
+
+// Отдаем игру каждому, кто просто перейдет по ссылке в браузере
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 let onlinePlayers = {};
@@ -10,23 +18,15 @@ let onlinePlayers = {};
 io.on('connection', (socket) => {
     console.log(`Игрок зашел в сеть: ${socket.id}`);
     
-    // Создаем персонажа для зашедшего игрока
     onlinePlayers[socket.id] = {
-        x: 100,
-        y: 200,
-        dir: 1,
+        x: 100, y: 200, dir: 1,
         name: "Кот_" + socket.id.substring(0, 4),
-        score: 0,
-        gameMode: "LOBBY"
+        score: 0, gameMode: "LOBBY"
     };
 
-    // Отправляем игроку карту и список тех, кто уже в лобби
     socket.emit('currentPlayers', onlinePlayers);
-    
-    // Оповещаем остальных, что зашел новый Кот
     socket.broadcast.emit('newPlayer', { id: socket.id, info: onlinePlayers[socket.id] });
 
-    // Постоянно принимаем координаты от каждого игрока
     socket.on('playerMovement', (movementData) => {
         if (onlinePlayers[socket.id]) {
             onlinePlayers[socket.id].x = movementData.x;
@@ -34,21 +34,16 @@ io.on('connection', (socket) => {
             onlinePlayers[socket.id].dir = movementData.dir;
             onlinePlayers[socket.id].gameMode = movementData.gameMode;
             onlinePlayers[socket.id].score = movementData.score;
-            
-            // Срочно транслируем его новые координаты всем остальным
             socket.broadcast.emit('playerMoved', { id: socket.id, info: onlinePlayers[socket.id] });
         }
     });
 
-    // Обработка выхода из игры
     socket.on('disconnect', () => {
-        console.log(`Игрок вышел: ${socket.id}`);
         delete onlinePlayers[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
 
-// Автоматический порт для хостингов (Render, Heroku и т.д.) или 3000 для ПК
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
     console.log(`[ONLINE] Сервер запущен на порту: ${PORT}`);
